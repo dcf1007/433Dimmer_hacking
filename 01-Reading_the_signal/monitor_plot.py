3b69d11b381567a9c6ef09c42ee7cfcc9f748405
dcf1007 <dcf1007@gmail.com>
2024-07-16 08:41:08 +0200

    Update monitor_plot.py
    Bugs fixed and improved

--- 01-Reading_the_signal/monitor_plot.py
+++ 01-Reading_the_signal/monitor_plot.py
@@ -17,7 +17,9 @@ parser.add_argument('GPIOs', metavar='N', type=int, nargs='*', help='GPIO pins t
 args = parser.parse_args()
 
 pi = pigpio.pi()
-timeout = 30 #Timeout in second
+timeout = 10 #Timeout in second
+data = {}
+plot = {}
 capture_finished = {}
 first = {}
 last = {}
@@ -26,12 +28,14 @@ cb = {}
 if not pi.connected:
    exit()
 
-if len(args["GPIOs"]) == 0:
+if len(args.GPIOs) == 0:
    G = range(0, 32)
 else:
-   G = args["GPIOs"]
+   G = args.GPIOs
 
 for g in G:
+   data[g] = []
+   plot[g] = []
    capture_finished[g] = False
    first[g] = None
    last[g] = None
@@ -41,17 +45,42 @@ capture_time = 1e6 #In micro-seconds
 def cbf(GPIO, level, tick):
    if first[GPIO] == None:
       first[GPIO] = tick
+   
    elif pigpio.tickDiff(first[GPIO], tick) < capture_time:
+      dFirst = pigpio.tickDiff(first[GPIO], tick)
+      
       if last[GPIO] != None:
+         dLast = pigpio.tickDiff(last[GPIO], tick)
+         
          # When rising it changes from 0 to 1
          if level == 1:
-            print(GPIO, pigpio.tickDiff(first[GPIO], tick), 0, pigpio.tickDiff(last[GPIO], tick))
-            print(GPIO, pigpio.tickDiff(first[GPIO], tick), 1, "")
+            data[GPIO].append((GPIO, dFirst, 0, dLast))
+            plot[GPIO].append((GPIO, dFirst, 0, dLast))
+            plot[GPIO].append((GPIO, dFirst, 1, ""))
+            print(GPIO, dFirst, 0, dLast)
+            print(GPIO, dFirst, 1, "")
+         
          # When falling it changes from 1 to 0
          else:
-            print(GPIO, pigpio.tickDiff(first[GPIO], tick), 1, pigpio.tickDiff(last[GPIO], tick))
-            print(GPIO, pigpio.tickDiff(first[GPIO], tick), 0, "")
-      last[GPIO] = tick
+            data[GPIO].append((GPIO, dFirst, 1, dLast))
+            plot[GPIO].append((GPIO, dFirst, 1, dLast))
+            plot[GPIO].append((GPIO, dFirst, 0, ""))
+            print(GPIO, dFirst, 1, dLast)
+            print(GPIO, dFirst, 0, "")
+         last[GPIO] = tick
+      
+      else:
+         # When rising it changes from 0 to 1
+         if level == 1:
+            plot[GPIO].append((GPIO, dFirst, 1, ""))
+            print(GPIO, dFirst, 1, "")
+         
+         # When falling it changes from 1 to 0
+         else:
+            plot[GPIO].append((GPIO, dFirst, 0, ""))
+            print(GPIO, dFirst, 0, "")
+         last[GPIO] = tick
+   
    else:
       capture_finished[GPIO] = True
 
@@ -62,14 +91,18 @@ for g in G:
 
 try:
    start_time = time.time()
+
    while all(capture_finished.values()) == False:
       if time.time() - start_time > timeout:
-         print("time out")
+         raise TimeoutError("Time out")
          break
       time.sleep(1)
-except KeyboardInterrupt:
-   print("\nTidying up")
+   else:
+      raise TimeoutError("Capturing done")
+except (KeyboardInterrupt, TimeoutError) as e:
+   print(e)
+   print("Tidying up")
+
    for c in cb.values():
       c.cancel()
-
-pi.stop()
+   pi.stop()
