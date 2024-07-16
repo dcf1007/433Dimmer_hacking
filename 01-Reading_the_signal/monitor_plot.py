50e94d2f5951633e93c77f555317be168bcf0e72
dcf1007 <dcf1007@gmail.com>
2024-07-16 07:43:18 +0200

    .

--- 01-Reading_the_signal/monitor_plot.py
+++ 01-Reading_the_signal/monitor_plot.py
@@ -7,9 +7,14 @@
 # monitor.py          # monitor all GPIO
 # monitor.py 23 24 25 # monitor GPIO 23, 24, and 25
 
-import sys
 import time
 import pigpio
+import argparse
+
+parser = argparse.ArgumentParser(description='Monitor GPIO activity and print it or save it to a file.')
+parser.add_argument('GPIOs', metavar='N', type=int, nargs='*', help='GPIO pins to monitor, separated by spaces')
+
+args = parser.parse_args()
 
 pi = pigpio.pi()
 timeout = 30 #Timeout in second
@@ -21,12 +26,10 @@ cb = {}
 if not pi.connected:
    exit()
 
-if len(sys.argv) == 1:
+if len(args["GPIOs"]) == 0:
    G = range(0, 32)
 else:
-   G = []
-   for a in sys.argv[1:]:
-      G.append(int(a))
+   G = args["GPIOs"]
 
 for g in G:
    capture_finished[g] = False
@@ -59,14 +62,14 @@ for g in G:
 
 try:
    start_time = time.time()
-   while all(capture_finished) == False:
+   while all(capture_finished.values()) == False:
       if time.time() - start_time > timeout:
          print("time out")
          break
       time.sleep(1)
 except KeyboardInterrupt:
    print("\nTidying up")
-   for c in cb:
+   for c in cb.values():
       c.cancel()
 
 pi.stop()
